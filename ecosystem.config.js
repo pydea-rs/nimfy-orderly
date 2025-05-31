6b2a08892a9c8413d5500ee7fb543ffd3a0e8307
pya-h <p4ya.py@gmail.com>
2025-05-31 05:20:10 +0330

    fixin fuckin pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,10 @@ export default {
   apps: [
     {
       name: "NincoFi",
-      script: "build",
+      script: "serve",
       args: "-s build -l 5173",
+      exec_mode: "fork", // important!
+      interpreter: "bash", // treat as shell command
       env: {
         NODE_ENV: "production",
       },
