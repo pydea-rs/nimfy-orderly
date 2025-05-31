9f9f6c6ed9b1a308282aba790dfb6036c017e767
pya-h <p4ya.py@gmail.com>
2025-05-31 05:21:43 +0330

    final try fixin serve problem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ export default {
   apps: [
     {
       name: "NincoFi",
-      script: "serve",
+      script: "/usr/local/bin/serve",
       args: "-s build -l 5173",
       exec_mode: "fork", // important!
       interpreter: "bash", // treat as shell command
